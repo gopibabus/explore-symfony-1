079500cc1f19b6d98b7bb5fae7f040ffa7db357c
gopibabu <s.gopibabu@gmail.com>
2020-12-20 13:17:26 -0600

    📦 NEW: added markdown support

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,12 +19,25 @@ class QuestionController extends AbstractController
 
     /**
      * @Route("/questions/{slug}", name="app_question_show")
+     * @param $slug
+     * @param MarkdownParserInterface $markdownParser
      * @return Response
      */
-    public function show($slug): Response
+    public function show($slug, MarkdownParserInterface $markdownParser): Response
     {
+        $answers = [
+            'Answer1 to the `Question`',
+            'Answer2 to the `Question`',
+            'Answer3 to the `Question`',
+            'Answer4 to the `Question`'
+        ];
+        $description = 'This is a **question** description';
+        $parsedDescription = $markdownParser->transformMarkdown($description);
+
         return $this->render('question/show.html.twig', [
-            'question' => $slug
+            'question' => $slug,
+            'answers' => $answers,
+            'description' => $parsedDescription
         ]);
     }
 }
