3fec0b6b0490cadb8b793fbe14b2d7bb074deea8
gopibabu <s.gopibabu@gmail.com>
2020-12-24 13:55:41 -0600

    added sentry to monitor error logs in the application

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Knp\Bundle\MarkdownBundle\KnpMarkdownBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
