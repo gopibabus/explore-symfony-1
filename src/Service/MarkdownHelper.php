def057a206082250a7e9f358df3a2ca1aa332301
gopibabu <s.gopibabu@gmail.com>
2020-12-24 13:30:56 -0600

    📦 NEW: explored services

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -2,6 +2,7 @@
 namespace App\Service;
 
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Cache\CacheInterface;
 
 class MarkdownHelper
@@ -16,13 +17,36 @@ class MarkdownHelper
      */
     private $cache;
 
-    public function __construct( MarkdownParserInterface $markdownParser, CacheInterface $cache){
+    /**
+     * @var bool
+     */
+    private $isDebug;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(
+        MarkdownParserInterface $markdownParser,
+        CacheInterface $cache,
+        bool $isDebug,
+        LoggerInterface $mdLogger
+    ){
         $this->markdownParser= $markdownParser;
         $this->cache = $cache;
+        $this->isDebug = $isDebug;
+        $this->logger = $mdLogger;
     }
 
     public function parse(string $source)
     {
+        if(stripos($source, 'cat') === false){
+            $this->logger->info('Sample Logger Info');
+        }
+        if($this->isDebug){
+            return $this->markdownParser->transformMarkdown($source);
+        }
         return $this->cache->get('markdown_'.md5($source), function()use($source){
             return $this->markdownParser->transformMarkdown($source);
         });
